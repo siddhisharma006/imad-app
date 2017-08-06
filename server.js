59811c357876aaba0d47c49fb22ccb5e5a64262f
siddhisharma006 <30735457+siddhisharma006@users.noreply.github.com>
2017-08-06 19:27:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/Article one', function (req, res) {
-   res.sendFile(path.join(__dirname, 'ui', 'Article one.html'));
+app.get('/Article-one', function (req, res) {
+   res.sendFile(path.join(__dirname, 'ui', 'Article-one.html'));
 });
     
 
