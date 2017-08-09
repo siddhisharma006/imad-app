b6a1411172efe81765a271ce4e06d5c781b19922
siddhisharma006 <30735457+siddhisharma006@users.noreply.github.com>
2017-08-10 00:32:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 
 // change the text of the main text .div
-var element= document.getElementById("main text"
+var element= document.getElementById('main text'
     );
-    element.innerHTML="SHARDHA";
+    element.innerHTML= 'SHARDHA' ;
     
