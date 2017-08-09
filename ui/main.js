94ac4f840e36a229f8c1068b5969edb174292396
siddhisharma006 <30735457+siddhisharma006@users.noreply.github.com>
2017-08-10 00:41:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,3 +5,8 @@ var element= document.getElementById('main text'
     );
     element.innerHTML= 'SHARDHA';
     
+    //move the image
+    var img= document.getElementById('madi' );
+   img.onclick = function () {
+        img.style.marginLeft='100px';
+    };
