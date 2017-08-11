3b71e666bdd37c7f4b94ed2984cb3e649e1e413e
siddhisharma006 <30735457+siddhisharma006@users.noreply.github.com>
2017-08-11 17:47:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,5 +8,5 @@ var element= document.getElementById('main text'
     //move the image
     var img= document.getElementById('madi' );
    img.onclick = function () {
-        img.style.marginLeft='10000px';
+        img.style.marginLeft='1000px';
     };
